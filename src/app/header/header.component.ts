ffb90e916d72c9fadc8c1933f2171638b8e65b5a
Eli <elionlink@gmail.com>
2022-01-28 03:58:21 +0200

    added header component with navigation logic and basic layout

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
+import { ActiveTab, routingModel, routingNames } from '../model/routing.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  routes$: Observable<{route: string, name: string, active: boolean}[]> | undefined;
+
+  constructor(private readonly router: Router) { }
+
+  ngOnInit(): void {
+    this.routes$ = this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd && event.snapshot.data['activeTab']),
+      map((event) => {
+      return [
+        {
+          route: routingModel.CONVERTER,
+          name: routingNames.CONVERTER,
+          active: (event as ActivationEnd).snapshot.data['activeTab'] === ActiveTab.CONVERTER,
+        },
+        {
+          route: routingModel.HISTORY,
+          name: routingNames.HISTORY,
+          active: (event as ActivationEnd).snapshot.data['activeTab'] === ActiveTab.HISTORY,
+        }
+      ]
+    }))
+  }
+}
