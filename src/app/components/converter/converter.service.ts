045591c5b3eda214c10abb3969a50d809f8a4e55
Eli <elionlink@gmail.com>
2022-01-28 03:55:34 +0200

    added converter service with logic to handle requests and transfer data

---
+++ src/app/components/converter/converter.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
+import { SessionStorageKeys } from 'src/app/app.consts';
+import { HttpService } from 'src/app/core/http.service';
+import { SessionStorageService } from 'src/app/core/session-storage.service';
+import { apiRoutes, ICurrenciesResponse } from 'src/app/model/api.model';
+import { ConvertionResponse, CurrenciesResponse, IConverterForm } from 'src/app/model/data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConverterService {
+  constructor(private readonly httpService: HttpService,
+              private readonly sessionStorage: SessionStorageService) { }
+
+  getCorrencies(): Observable<CurrenciesResponse[]> {
+    const fromSession = this.sessionStorage.readItem(SessionStorageKeys.CORRENCIES);
+    if (fromSession) return of(fromSession);
+    return this.httpService.get(apiRoutes.CURRENCIES).pipe(
+      map((data) => {
+        return this.mapObjectToArray(data);
+      }),
+      tap((data) => this.sessionStorage.setItem(SessionStorageKeys.CORRENCIES, data))
+    );
+  }
+
+  getConvertionRate(request: IConverterForm): Observable<ConvertionResponse> {
+    const url = `latest?amount=${request.amount}&from=${request.from}&to=${request.to}`;
+    return this.httpService.get(url).pipe(
+      map(({rates}) => {
+        return new ConvertionResponse(request,rates[request.to]);
+      })
+    )
+  }
+
+  mapObjectToArray(object: ICurrenciesResponse): CurrenciesResponse[] {
+    const array: CurrenciesResponse[] = [];
+    for (const key in object) {
+      array.push(new CurrenciesResponse({[key]: object[key]}));
+    }
+    return array;
+  }
+}
