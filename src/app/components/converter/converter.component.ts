efa51b86dd8717cd5ef5d81f3c763adb88176a7b
Eli <elionlink@gmail.com>
2022-01-28 16:21:50 +0200

    prevent source and target currency to be the same

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { debounceTime, filter, Observable, switchMap, tap } from 'rxjs';
-import { ConvertionResponse } from 'src/app/model/data.model';
-import { HistoryService } from '../history/history.service';
 import { ConverterService } from './converter.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HistoryService } from '../history/history.service';
+import { ConvertionResponse } from 'src/app/model/data.model';
+import { debounceTime, filter, Observable, switchMap, tap } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-converter',
@@ -16,11 +16,12 @@ export class ConverterComponent implements OnInit {
   currentRate$: Observable<ConvertionResponse> | undefined;
   currencies$ = this.converterService.getCorrencies();
   converterDebounceTime = 1000;
+
   form = this.fb.group({
-    amount: [1,[Validators.required]],
+    amount: [1,Validators.required],
     from: [null, Validators.required],
     to: [null, Validators.required],
-  })
+  });
 
   constructor(private readonly converterService: ConverterService,
               private readonly historyService: HistoryService,
@@ -33,10 +34,12 @@ export class ConverterComponent implements OnInit {
   subToFormChanges(): void {
    this.currentRate$ = this.form.valueChanges.pipe(
       debounceTime(this.converterDebounceTime),
-      filter(() => this.form.valid && (this.form.value['amount'] > 0)),
+      filter(() => this.form.valid),
+      filter(() => this.form.value['amount'] > 0),
+      filter(() => this.form.value['from'] !== this.form.value['to']),
       switchMap(() => this.converterService.getConvertionRate(this.form.getRawValue())),
       tap((converted) => this.historyService.addHistory(converted)),
-    )
+    );
   }
 
 }
