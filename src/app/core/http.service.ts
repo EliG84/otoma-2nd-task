6384a5a200652e044c6f1ccfcac4b4744ecd1bbc
Eli <elionlink@gmail.com>
2022-01-28 03:53:22 +0200

    added core http service

---
+++ src/app/core/http.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private baseUrl = '';
+  constructor(private readonly httpClient: HttpClient) {
+    this.baseUrl = environment.BASE_URL;
+   }
+
+  get(url: string): Observable<any> {
+    return this.httpClient.get<any>(`${this.baseUrl}/${url}`).pipe(
+      map(this.handleResponse),
+      catchError(() => of(null))
+    )
+  }
+
+  handleResponse(response: HttpResponse<any>): any | HttpResponse<any> {
+    if (response.body && response.body.hasOwnProperty('data')) {
+      return response.body.data;
+    }
+    return response;
+  }
+}
